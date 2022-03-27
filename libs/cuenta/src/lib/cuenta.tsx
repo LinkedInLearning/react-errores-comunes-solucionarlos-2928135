f1145939772e54950fa14215753d5c29c65cc4ca
Sergio Brito <sergiobritor@gmail.com>
2022-03-27 19:59:13 -0400

    feat(02_02): set datatype properties

--- libs/cuenta/src/lib/cuenta.tsx
+++ libs/cuenta/src/lib/cuenta.tsx
@@ -8,7 +8,7 @@ export function Cuenta(props: CuentaProps) {
   return (
     <div>
       <h1>Welcome to Cuenta!</h1>
-      <Summary quantity="1" />
+      <Summary quantity={1} total={10} />
     </div>
   );
 }
--- libs/cuenta/src/lib/summary/summary.tsx
+++ libs/cuenta/src/lib/summary/summary.tsx
@@ -1,7 +1,12 @@
 import { Component } from 'react';
 import './summary.module.scss';
 
-class Summary extends Component {
+interface SummaryProps {
+  quantity: number;
+  total: number;
+}
+
+class Summary extends Component<SummaryProps> {
   render() {
     return (
       <h1>Elegiste {this.props.quantity === 1 ? 'platillo' : 'platillos'} .</h1>
