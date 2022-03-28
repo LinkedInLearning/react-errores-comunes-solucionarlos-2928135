c794bb842b192a4c89af8ddecfcb419fe74c1a27
Sergio Brito <sergiobritor@gmail.com>
2022-03-27 21:09:42 -0400

    feat(02_04): use generics

--- libs/cuenta/src/lib/summary/summary.tsx
+++ libs/cuenta/src/lib/summary/summary.tsx
@@ -6,19 +6,31 @@ interface SummaryProps {
   total: number;
 }
 
-type ClientType = 'regular' | 'frequent';
-
-interface Customer {
+interface User {
   name: string;
   email: string;
+}
+
+interface FullUser {
+  name: string;
+  email: string;
+  city: string;
+  age: number;
+  tShirtSize: string;
+  branch: string;
+}
+
+type ClientType = 'regular' | 'frequent';
+
+interface Customer<T> {
+  info: T;
   type: ClientType;
 }
 
 type Level = 'primary' | 'occasional';
 
-interface Provider {
-  name: string;
-  email: string;
+interface Provider<T> {
+  info: T;
   phone: string;
   level: Level;
   pendingBill: number;
@@ -26,6 +38,45 @@ interface Provider {
 }
 
 class Summary extends Component<SummaryProps> {
+  sampleUser: Customer<string> = {
+    info: 'este es un usuario de prueba',
+    type: 'regular',
+  };
+
+  currentCustomer: Customer<User> = {
+    info: {
+      name: 'Checo',
+      email: 'checo@mail.com',
+    },
+    type: 'frequent',
+  };
+
+  currentProvider: Provider<User> = {
+    info: {
+      name: 'Clark',
+      email: 'clark@mail.com',
+    },
+    phone: '55555555',
+    level: 'primary',
+    pendingBill: 123000,
+    active: true,
+  };
+
+  fullProvider: Provider<FullUser> = {
+    info: {
+      name: 'Clark',
+      email: 'clark@mail.com',
+      city: 'CDMX',
+      age: 30,
+      tShirtSize: 'XL',
+      branch: 'Condesa',
+    },
+    phone: '55555555',
+    level: 'primary',
+    pendingBill: 123000,
+    active: true,
+  };
+
   render() {
     return (
       <h1>Elegiste {this.props.quantity === 1 ? 'platillo' : 'platillos'} .</h1>
